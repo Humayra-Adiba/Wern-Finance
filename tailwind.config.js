2601809d43228fe786e4689e4cce1a69ba7219ab
Humayra-Adiba <tahsinadiba2024@outlook.com>
2025-01-05 17:40:34 +0600

    bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-const {nextui} = require("@nextui-org/react");
+// /** @type {import('tailwindcss').Config} */
+// const {nextui} = require("@nextui-org/react");
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+//   darkMode: "class",
+//   plugins: [nextui()],
+// }
+
+
+import {nextui} from "@nextui-org/react";
+
 export default {
   content: [
     "./index.html",
